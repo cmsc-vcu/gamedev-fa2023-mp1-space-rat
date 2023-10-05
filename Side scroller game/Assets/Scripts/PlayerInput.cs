56456c6dce0bc05a11afb41a1883fdfb82ffeca4
debruler <debrulerij@vcu.edu>
2023-10-04 21:02:56 -0400

    scrolling background wip

--- Side scroller game/Assets/Scripts/PlayerInput.cs
+++ Side scroller game/Assets/Scripts/PlayerInput.cs
@@ -13,18 +13,25 @@ public class PlayerInput : MonoBehaviour
     public TMP_Text scoreText;
     public static int scoreNum = 0;
     public GameObject scoreTracker;
+    public Camera cam;
+    float screenHeight;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gameObject.tag = "Player";
         scoreText.text = "score: 0";
+        //Camera.main.orthographic = true;
     }
 
     
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + change * speed * Time.fixedDeltaTime);
+        if (rb.position.y <= cam.orthographicSize + 2.5)
+        {
+            rb.MovePosition(rb.position + change * speed * Time.fixedDeltaTime);
+        }
     }
     
 
@@ -32,6 +39,10 @@ public class PlayerInput : MonoBehaviour
     {
         verticalInput = Input.GetAxisRaw("Vertical");
         change = new Vector2(0, verticalInput);
+        Debug.Log(2 * cam.orthographicSize);
+        Debug.Log(rb.position.y);
+
+
 
     }
 
